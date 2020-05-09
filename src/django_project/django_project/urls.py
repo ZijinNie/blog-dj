3bb5477e52b8a01be2062fab31c86419a30ed1ca
ZijinNie <zijin.nie@mail.mcgill.ca>
2020-05-09 14:09:23 -0400

    add profile function and add signal for creating profiles

--- src/django_project/django_project/urls.py
+++ src/django_project/django_project/urls.py
@@ -19,6 +19,9 @@ from users.views import (register,
 	profile)
 from blog.views import home
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
 	path('', home, name='home-page'),
     path('admin/', admin.site.urls),
@@ -28,3 +31,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name = "login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name = "logout"),
 ]
+
+if settings.DEBUG:
+
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
